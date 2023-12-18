9224266efb0ea0fd0f9a35aa8f8b0053fbbb43a5
Brian <bstuart777@gmail.com>
2023-12-17 20:56:55 -0500

    Bezier graph setup

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 
-export default function App() {
+const MyLineChart = () => {
   return (
+    <>
+      <Text style={styles.header}>Line Chart</Text>
+      <LineChart
+        data={{
+          labels: ['12/9', '12/10', '12/11', '12/12', '12/13', '12/14', '12/15'],
+          datasets: [
+            {
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, //point color and line color
+              data: [1, 2, 0, 3, 2, 5, 0],
+              strokeWidth: 2, //chart line thickness
+            },
+          ],
+          legend: ["  1 - Sad 2 - Mad, 3 - Meh, 4 - Happy, 5 - Excited"],
+        }}
+        width={Dimensions.get('window').width}
+        height={Dimensions.get('window').height - 500}
+        chartConfig={{
+          backgroundGradientFrom: '#FEF3E7', //for some reason need this, background color and color are useless
+          backgroundGradientTo: '#FEF3E7',
+          decimalPlaces: 0,                 //after whole value
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})` //dotted line color (also changes text x labels and key)
+        }}
+        bezier
+      />
+    </>
+  );
+};
+
+const App = () => {
+  return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <MyLineChart />
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
   },
 });
